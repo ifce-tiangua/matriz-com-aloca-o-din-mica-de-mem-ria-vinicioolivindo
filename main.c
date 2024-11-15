778f2c64696bf944126766e482313ca3ae72b074
v1nny:) <vinicioolivindo81@gmail.com>
2024-11-15 17:56:17 -0300

    funciona vai

--- main.c
+++ main.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+void imprimir_matriz( int **mat, int colunas, int linhas){
+    if (linhas == 0 || colunas == 0){
+        printf("[matriz vazia]");
+        return;
+    }
+    for(int l = 0; l < linhas; l++){
+        for(int c = 0; c < colunas; c++){
+            printf("%d", mat[l][c]);
+        }
+        printf("\n");
+    }
+}
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+int main()
+{
+
+    int lin, col;
+
+    scanf("%d", &lin);
+    scanf("%d", &col);
+
+    int **matriz;
+
+    matriz = (int **)malloc(lin * sizeof(int *));
+    for (int i = 0; i < lin; i++)
+        matriz[i] = (int *)malloc(col * sizeof(int));
+
+    for (int l = 0; l < lin; l++)
+    {
+        for (int c = 0; c < col; c++)
+        {
+            scanf("%d", &matriz[l][c]);
+        }
+    }
+
+    imprimir_matriz(matriz, col, lin);
+
 
     return 0;
 }
